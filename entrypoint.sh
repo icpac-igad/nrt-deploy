109eb5c9d0fedccbdbbda14864099517ed3e5742
Erick Otenyo <otenyo.erick@gmail.com>
2021-09-29 13:24:10 +0300

    Modify scripts

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,28 +7,30 @@ LOG=${LOG:-udp://localhost}
 # fetch repo
 echo "pulling repository"
 if [ -d "$NRT_DIR" ]; then
-    cd $NRT_DIR
-    git pull origin main
+  cd $NRT_DIR
+  git pull origin main
 else
-    git clone $NRT_REPO
-    cd $NRT_DIR
+  git clone $NRT_REPO
+  cd $NRT_DIR
 fi
 
 # set up crontab
 echo "Creating cronfile"
 rm -f crontab
+
+# add line to enable sending email after cron job finishes
+echo "MAILTO='eotenyo@icpac.net'" >>cron.tmp
+
 find . -name "*.cron" | while read fname; do
-    DIR=$(basename $(dirname $fname))
-    cp -f ../.env $DIR
-    chmod +x $DIR/start.sh
-    mkdir -p $DIR/data
-    if [ -z $CRONNOW ]
-    then
-        CRON=$(cat $fname)
-    else
-        CRON="$(expr $(date +%M) + 2) $(date +%H) * * *"
-    fi
-    echo "$CRON cd $(pwd)/$DIR && LOG=$LOG ./start.sh >> /var/log/cron 2>&1" >> cron.tmp
+  DIR=$(basename $(dirname $fname))
+  cp -f ../.env $DIR
+  chmod +x $DIR/start.sh
+  if [ -z $CRONNOW ]; then
+    CRON=$(cat $fname)
+  else
+    CRON="$(expr $(date +%M) + 2) $(date +%H) * * *"
+  fi
+  echo "$CRON cd $(pwd)/$DIR && LOG=$LOG ./start.sh >> /var/log/cron 2>&1" >>cron.tmp
 done
 
 crontab cron.tmp
--- run.sh
+++ run.sh
@@ -1,2 +1,1 @@
-docker-compose build && docker-compose up
-
+docker-compose build && docker-compose up -d
